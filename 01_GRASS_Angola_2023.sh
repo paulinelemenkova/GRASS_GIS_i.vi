c16c8cab17b61f55acf4a1375b0094a44bc8b722
Polina Lemenkova <pauline.lemenkova@gmail.com>
2023-11-26 14:53:29 +0100

    Add files via upload

--- 01_GRASS_Angola_2023.sh
+++ 01_GRASS_Angola_2023.sh
@@ -4,17 +4,17 @@ r.info -r LC08_L2SP_189056_20231220_20200912_02_T1_SR_B1
 # checking the selected random band
 gdalinfo LC08_L2SP_189056_20231220_20200912_02_T1_SR_B7.TIF
 # importing the image subset with 7 Landsat bands and display the raster map (polinalemenkova)
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_SR_B1.TIF out=L8_2023_01
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_SR_B2.TIF out=L8_2023_02
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_SR_B3.TIF out=L8_2023_03
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_SR_B4.TIF out=L8_2023_04
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_SR_B5.TIF out=L8_2023_05
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_SR_B6.TIF out=L8_2023_06
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_SR_B7.TIF out=L8_2023_07
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_ST_EMIS.TIF out=L8_2023_08
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_ST_EMSD.TIF out=L8_2023_09
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_ST_B10.TIF out=L8_2023_10
-r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230716_20200912_02_T1_ST_TRAD.TIF out=L8_2023_11
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B1.TIF out=L8_2023_01
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B2.TIF out=L8_2023_02
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B3.TIF out=L8_2023_03
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B4.TIF out=L8_2023_04
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B5.TIF out=L8_2023_05
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B6.TIF out=L8_2023_06
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B7.TIF out=L8_2023_07
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_ST_EMIS.TIF out=L8_2023_08
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_ST_EMSD.TIF out=L8_2023_09
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_SR_B10.TIF out=L8_2023_10
+r.in.gdal /Users/polinalemenkova/grassdata/Angola_2023/LC08_L2SP_178069_20230712_20230718_02_T1_ST_TRAD.TIF out=L8_2023_11
 
 # listing the files
 g.list rast
@@ -33,8 +33,8 @@ g.copy raster=L8_2023_10,lsat8_2023.10
 g.copy raster=L8_2023_11,lsat8_2023.11
 # converting the DN pixel values to reflectance values using DOS1. From Digital Numer (DN) to reflectance. Before creating an RGB composite, it is important to convert the digital number data (DN) to reflectance (or optionally radiance). Otherwise the colors of a “natural” RGB composite do not look convincing but rather hazy (see background in the next screenshot). This conversion is done using the metadata file which is included in the data set with i.landsat.toar # i.landsat.toar - Calculates top-of-atmosphere radiance or reflectance and temperature for Landsat MSS/TM/ETM+/OLI
 i.landsat.toar input=lsat8_2023. output=lsat8_2023_toar. sensor=oli8 \
-    method=dos1 date=2013-07-16 sun_elevation=44.88556697 \
-    product_date=2020-09-12 gain=HHHLHLHHL --overwrite
+    method=dos1 date=2023-07-12 sun_elevation=44.08803962 \
+    product_date=2023-07-18 gain=HHHLHLHHL --overwrite
 #
 # Calculation of VI
 #
